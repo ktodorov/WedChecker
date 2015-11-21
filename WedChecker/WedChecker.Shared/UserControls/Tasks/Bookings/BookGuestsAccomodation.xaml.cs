f3abd7377578f67b0866c5b73f62cd4f5114bc11
ktodorov <kztodorov@outlook.com>
2015-11-21 18:31:42 +0200

    Added BookGuestsAccomodation booking task

---
+++ WedChecker/WedChecker.Shared/UserControls/Tasks/Bookings/BookGuestsAccomodation.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using WedChecker.Common;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WedChecker.UserControls.Tasks.Bookings
+{
+    public sealed partial class BookGuestsAccomodation : BaseTaskControl
+    {
+        private List<string> _storedAccomodationPlaces;
+
+        public List<string> StoredAccomodationPlaces
+        {
+            get
+            {
+                if (_storedAccomodationPlaces == null)
+                {
+                    _storedAccomodationPlaces = AppData.GetGlobalValues(TaskData.Tasks.AccomodationPlaces.ToString());
+                }
+
+                return _storedAccomodationPlaces;
+            }
+        }
+
+        public override string TaskName
+        {
+            get
+            {
+                return "Accomodation places";
+            }
+        }
+
+        public override string EditHeader
+        {
+            get
+            {
+                return "What have you booked so far?";
+            }
+        }
+
+        public override string DisplayHeader
+        {
+            get
+            {
+                return "This is what you have booked so far";
+            }
+        }
+
+        public BookGuestsAccomodation()
+        {
+            this.InitializeComponent();
+
+            foreach (var accomodationPlace in StoredAccomodationPlaces)
+            {
+                var toggle = new ToggleControl();
+                toggle.Title = accomodationPlace;
+                AddToggle(toggle);
+            }
+        }
+
+        public BookGuestsAccomodation(Dictionary<string, bool> accomodationPlaces)
+        {
+            this.InitializeComponent();
+
+            foreach (var accomodationPlace in accomodationPlaces)
+            {
+                var toggle = new ToggleControl(accomodationPlace.Key, accomodationPlace.Value);
+                AddToggle(toggle);
+            }
+
+            var remainingStoredAccomodationPlaces = StoredAccomodationPlaces.Where(sa => !accomodationPlaces.Any(a => a.Key == sa));
+            foreach (var accomodationPlace in remainingStoredAccomodationPlaces)
+            {
+                var toggle = new ToggleControl(accomodationPlace);
+                AddToggle(toggle);
+            }
+        }
+
+        public override void DisplayValues()
+        {
+            var toggles = mainPanel.Children.OfType<ToggleControl>();
+            foreach (var toggle in toggles)
+            {
+                toggle.DisplayValues();
+            }
+        }
+
+        public override void EditValues()
+        {
+            var toggles = mainPanel.Children.OfType<ToggleControl>();
+            foreach (var toggle in toggles)
+            {
+                toggle.EditValues();
+            }
+        }
+
+        public override void Serialize(BinaryWriter writer)
+        {
+            writer.Write(TaskData.Tasks.BookGuestsAccomodation.ToString());
+
+            var toggles = mainPanel.Children.OfType<ToggleControl>();
+            writer.Write(toggles.Count());
+            foreach (var toggle in toggles)
+            {
+                toggle.Serialize(writer);
+            }
+        }
+
+        public override void Deserialize(BinaryReader reader)
+        {
+            var count = reader.ReadInt32();
+
+            for (var i = 0; i < count; i++)
+            {
+                var toggle = new ToggleControl();
+                toggle.Deserialize(reader);
+
+                AddToggle(toggle);
+            }
+
+            DisplayValues();
+        }
+
+        public override async Task SubmitValues()
+        {
+            await AppData.InsertGlobalValue(TaskData.Tasks.BookGuestsAccomodation.ToString());
+        }
+
+        private void AddToggle(ToggleControl toggle)
+        {
+            if (!StoredAccomodationPlaces.Contains(toggle.Title))
+            {
+                return;
+            }
+
+            var allToggles = mainPanel.Children.OfType<ToggleControl>();
+
+            if (allToggles.Any(t => t.Title == toggle.Title))
+            {
+                mainPanel.Children.OfType<ToggleControl>().FirstOrDefault(t => t.Title == toggle.Title).Toggled = toggle.Toggled;
+                return;
+            }
+
+            mainPanel.Children.Add(toggle);
+        }
+    }
+}
