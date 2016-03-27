0adac4ad04ad18564b5908d62e6b657628654802
ktodorov <kztodorov@outlook.com>
2016-03-27 21:58:56 +0300

    Associated the Windows 10 version with the store

--- WedChecker/App.xaml.cs
+++ WedChecker/App.xaml.cs
@@ -46,7 +46,7 @@ namespace WedChecker
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                //this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
 
